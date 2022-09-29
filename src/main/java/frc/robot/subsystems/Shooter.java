1a2d33c2ad0b55e4bab8ac61f4358340c4940a2f
ExoMusubi <112046931+ExoMusubi@users.noreply.github.com>
2022-09-29 00:55:00 +0000

    Smart dash board changed

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,19 +1,41 @@
 package frc.robot.subsystems;
 
+import java.util.Set;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public Shooter() {}
+  private CANSparkMax motor = MotorControllerFactory.createSparkMax();
+
+  public Shooter() 
+  {
+    SmartDashboard.putnumber("Shooter motor speed", 0);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     
+    motor.set(SmartDashboard.getnumber("Shooter motor speed", 0));
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+  // spped of which the ball is fired
+  public void setShootingSpeed()
+{
+
+  
+}
+
+public void angleChange()
+{
+  
+}
+
+
 }
